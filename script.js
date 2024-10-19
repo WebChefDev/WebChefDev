81d5ce36579bbcc3f5f8e7572d2e6af2e0c94433
WebChefDev <webchefdev@gmail.com>
2024-10-19 15:13:04 -0600

    counter

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ function closeWebsite() {
   iframe.style.display = "none";
   document.getElementById("closeButton").style.display = "none";
 }
+
+
+
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  document.getElementById(
+    "clock"
+  ).textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+updateClock();
